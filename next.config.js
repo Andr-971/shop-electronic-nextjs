a9590f3820dab7a0dd1b9b13f4f582cad18fe184
Андрей <andrei.ryabov2020@gmail.com>
2024-08-07 16:29:54 +0500

    start-start

---
+++ next.config.js
@@ -0,0 +1,40 @@
+
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
+
+module.exports = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, "scss")],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [require("postcss-preset-env")],
+                            },
+                        },
+                    },
+                    "group-css-media-queries-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "css/[name]-[contenthash].css",
+            }),
+        ],
+    },
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,11 @@
 
 /** @type {import('next').NextConfig} */
-// const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const postcssPresetEnv = require("postcss-preset-env");
-const allMedia = require("postcss-combine-media-query");
 
 const mode = process.env.NODE_ENV || "development";
 const devMode = mode === "development";
 const target = devMode ? "web" : "browserslist";
 const devtool = devMode ? "source-map" : undefined;
 
-// module.exports = {
-//     sassOptions: {
-//         includePaths: [path.join(__dirname, "scss")],
-//     },
-// };
 const nextConfig = {
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         
@@ -31,37 +22,8 @@ const nextConfig = {
                     },
                 },
             },
-            {
-                test: /\.(c|sa|sc)ss$/i,
-                use: [
-                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                postCssPlugins: [
-                                    require("postcss-preset-env"),
-                                    require("postcss-logical"),
-                                ],
-                            },
-                        },
-                    },
-                    "group-css-media-queries-loader",
-                    "sass-loader",
-                ],
-            }
+            
         );
-        config = {
-            sassOptions: {
-                includePaths: [path.join(__dirname, "scss")],
-            },
-            plugins: [
-                new MiniCssExtractPlugin({
-                    filename: ".next/static/css/style.css",
-                }),
-            ],
-        };
         return config;
     },
 };
