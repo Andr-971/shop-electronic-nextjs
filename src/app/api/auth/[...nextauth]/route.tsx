efcf848229d2aa3514a9a1166e928c3767e7f799
Андрей <andrei.ryabov2020@gmail.com>
2024-11-26 11:40:44 +0500

    26.11.24end1

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,8 +1,13 @@
 import NextAuth from "next-auth";
 import { authConfig } from "../../../../configs/auth";
 
+export async function generateStaticParams() {
+    return [
+        "https://github.com/settings/apps",
+        "https://oauth.yandex.com/client/new",
+    ];
+}
 
 const handler = NextAuth(authConfig);
-
 
 export { handler as GET, handler as POST };
