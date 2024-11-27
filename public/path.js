ce7e1c9543815e32ad81cb361d94ef1b31f23901
Андрей <andrei.ryabov2020@gmail.com>
2024-11-27 15:27:59 +0500

    27.11.24end4

--- public/path.js
+++ public/path.js
@@ -1,5 +1,6 @@
 
-const domen = `127.0.0.1:3000`;
+const domen = `https://shop-electronic-nextjs.vercel.app`;
 // 127.0.0.1:3000
 // http://localhost:3000
+// https://shop-electronic-nextjs.vercel.app
 export { domen };
